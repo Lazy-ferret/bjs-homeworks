b8b4b4686c725502e309d363ba9b94cc5210a6cb
Alexander Veselov <a.veselov83@gmail.com>
2020-08-29 23:12:47 +0300

    1.3-data-types homework done

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,27 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+  'use strict';
+  if (isNaN(percent)){
+    return `Параметр \"Процентная ставка\" содержит неправильное значение ${percent}`; 
+  }
+  if (isNaN(contribution)){
+    return `Параметр \"Начальный взнос\" содержит неправильное значение ${contribution}`; 
+  }
+  if (isNaN(amount)){
+    return `Параметр \"Общая стоимость\" содержит неправильное значение ${amount}`; 
+  }
+
+  let mainDebt = amount - contribution;
+  let numberOfMonths = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+  let p = percent / 100 / 12;
+  let paymentAmount = mainDebt * (p + p / (((1 + p) ** numberOfMonths) - 1));
+  let totalAmount = +(paymentAmount * numberOfMonths).toFixed(2);
+  return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+  if ((name == false) || (name == null)) {
+    name = 'Аноним';
+  } 
+  let greeting = `Привет, мир! Меня зовут ${name}`;
+  return greeting;
 }
